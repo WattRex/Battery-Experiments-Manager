6e9ef69e45ebdf1165aafafe6328716b3272411b
Javibu13 <javiersanzmoline@gmail.com>
2023-10-31 11:20:41 +0100

    Clean branch mn_manager

--- auto_lab/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AutoLabConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'auto_lab'
--- auto_lab/templatetags/underscore_to_space.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def underscore_to_space(value):
-    return value.replace("_"," ")
