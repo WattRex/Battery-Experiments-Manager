8d96e60f86351a2b2f981ec7963e7366552ea181
Manager <manager@server.es>
2023-11-02 09:28:07 +0100

    Changes requested in pull_request review

--- mn_manager/src/wattrex_mn_manager/__init__.py
+++ mn_manager/src/wattrex_mn_manager/__init__.py
@@ -4,8 +4,5 @@ web server with CUs.
 '''
 
 from .mn_manager_node import MN_DATA_CHAN_NAME, MN_REQS_CHAN_NAME, MnManagerNodeC
-from .mn_broker_client import BrokerClientC
-from .mn_db_facade import DbFacadeC
 
-__all__ = ['MN_DATA_CHAN_NAME', 'MN_REQS_CHAN_NAME', 'MnManagerNodeC', 'BrokerClientC',
-                        'DbFacadeC']
+__all__ = ['MN_DATA_CHAN_NAME', 'MN_REQS_CHAN_NAME', 'MnManagerNodeC']
